09e4f4cb2fcec1a15461cc16c7f5b8eb30af5b91
Miroslava Vybiralova <miroslava.vybiralova@gmail.com>
2024-01-31 20:19:24 +0100

    Pridani ukolu jedna

---
+++ script.js
@@ -0,0 +1,16 @@
+// Lekce 2 úkoly
+
+/* Vymyslete, jak použít funkci Math.random a různé zaokrouhlovací funkce probírané
+ v této lekci k simulování hodu klasickou šestistěnnou kostkou. 
+ S použitím vhodných funkcí sestavte výraz, který vygeneruje náhodné celé číslo mezi 1 a 6.
+ 
+Zamyslete se nad tím, zda vámi vytvořený výraz generuje všechna čísla skutečně se stejnou
+ pravděpodobností. Vemte v úvahu, že funkce Math.random generuje náhodná čísla mezi 
+ 0 (včetně) a 1 (vyjma). Je tedy malinká pravěpodobnost, že občas padne přesně číslo 0. 
+ Naopak číslo 1 padnout nemůže.
+ */
+
+ document.body.innerHTML += "Hod kostkou je: " + Math.floor((Math.random()*6)+1)
+
+
+
